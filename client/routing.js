6928356c16972a2286b25f677fe92c15f2153cd7
sohelvali <sohel@cakewalk.in>
2015-04-10 23:11:04 +0530

    routing add and add pages
    deleteNote

---
+++ client/routing.js
@@ -0,0 +1,17 @@
+Router.configure({
+    layoutTemplate:'main'
+});
+
+Router.map(function(){
+    this.route('NoteList',{path:'/'});
+
+    this.route('group',{path:'/group'});
+
+
+    this.route('contactUs', {path:'/contactUs'});
+
+    this.route('aboutUs', {path:'/aboutUs'});
+
+    this.route('help', {path:'/help'});
+
+});
